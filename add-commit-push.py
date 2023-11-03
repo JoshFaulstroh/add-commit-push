f02030068b3ea46bfdd1def4da62e87a04348ada
JoshFaulstroh <joshfaulstroh@gmail.com>
2023-11-03 10:49:32 -0500

    Safe Code

--- add-commit-push.py
+++ add-commit-push.py
@@ -1,10 +1,29 @@
-
 import os
+import sys
 
-print("Add Commit Push") 
+print("Add Commit Push Starting...") 
+numOfArgs = len(sys.argv)
+print("Total Arguments Passed:", numOfArgs)
+message = "Default commit"
+if numOfArgs == 3:
+    if sys.argv[1] == "-m":    
+        print("There are 3 arguments, p2 = -m")
+    message = sys.argv[2] 
+
+print(message)
+
+print("Enter Your Name:")
+x = input()
+print("Hello,", x)
 
 os.system("git status")
+print("Continue? type 'y' for yes, 'n' for no.")
+confirm = input()
+if confirm != "y":
+    print("terminating...")
+    quit()
+
 os.system("git add .")
 os.system("git status")
-os.system("git commit -m ''")
+os.system("git commit -m 'Default commit'")
 os.system("git push")

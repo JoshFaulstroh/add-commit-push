eb3e96ad13abc4e12b220fdab8836e83c2a4b056
JoshFaulstroh <joshfaulstroh@gmail.com>
2023-11-04 22:06:07 -0500

    q

--- add-commit-push.py
+++ add-commit-push.py
@@ -2,28 +2,39 @@ import os
 import sys
 import time
 
-print("Starting...")
+print("\nStarting...")
 time.sleep(0.5) 
 numOfArgs = len(sys.argv)
-print("Total Arguments Passed:", numOfArgs)
+print("\nTotal Arguments Passed:", numOfArgs)
 
-os.system("git status")
-print("Continue? Y or N")
+print("\ngit status")
+os.system("\ngit status")
+print("\nContinue? Y or N")
 confirm = input()
 if confirm == "n":
-    print("terminating...")
+    print("\nterminating...")
+    time.sleep(1)
     quit()
 
-message = input("Please provide commit message:")
-print("Commit message entered:", message, "Is this correct? Y or N")
+message = input("\nPlease provide commit message:")
+print("\nCommit message entered:", message, "Is this correct? Y or N")
 messageconfirm = input()
 if messageconfirm == "n":
-    correctedMessage = input("please provide commit message:")
+    correctedMessage = input("\nplease provide commit message:")
 
 
 commitM = '\ngit commit -m "' +  message + '"'
+print("\ngit commit", message)
+time.sleep(0.5)
 os.system("\n"+commitM)
+print("\ngit add .")
+time.sleep(0.5)
 os.system("\ngit add .")
+print("\ngit status")
+time.sleep(0.5)
 os.system("\ngit status")
+print("\ngit push")
+time.sleep(0.5)
 os.system("\ngit push")
 print("\n...Finished")
+time.sleep(0.5)

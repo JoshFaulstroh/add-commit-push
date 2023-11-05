9262c24756e065be2422eb2f35c9b21562a71b81
JoshFaulstroh <joshfaulstroh@gmail.com>
2023-11-05 07:38:57 -0600

    q

--- add-commit-push.py
+++ add-commit-push.py
@@ -7,7 +7,7 @@ time.sleep(0.5)
 numOfArgs = len(sys.argv)
 print("\nTotal Arguments Passed:", numOfArgs)
 
-print("\ngit status")
+print("\ngit status\n")
 time.sleep(0.5)
 os.system("\ngit status")
 time.sleep(0.5)
@@ -18,30 +18,53 @@ if confirm == "n":
     time.sleep(1)
     quit()
 
-message = input("\nPlease provide commit message:")
+message = input("\nPlease provide commit message: ")
 print("\nCommit message entered:", message, "Is this correct? Y or N\n")
 messageconfirm = input()
 if messageconfirm == "n":
-    correctedMessage = input("\nplease provide commit message:")
+    correctedMessage = input("\nplease provide commit message: ")
 
 
 commitM = '\ngit commit -m "' +  message + '"'
 time.sleep(0.5)
+print("\nYour changes are about to be committed to your repository. Continue? Y or N\n")
+commitConfirm = input()
+if commitConfirm == "n":
+    print("\nterminating...\n")
+    time.sleep(1)
+    quit()
+
 print("\ngit commit", message, "\n")
 time.sleep(0.5)
 os.system("\n"+commitM)
 time.sleep(0.5)
+
+print("\nYou are about to perform the git add command. Continue? Y or N\n")
+addConfirm = input()
+if addConfirm == "n":
+    print("\nterminating...\n")
+    time.sleep(1)
+    quit()
 print("\ngit add .\n")
 time.sleep(0.5)
 os.system("\ngit add .")
 time.sleep(0.5)
+
 print("\ngit status\n")
 time.sleep(0.5)
 os.system("\ngit status")
 time.sleep(0.5)
+
+print("\nYou are about to perform the git push command. Continue? Y or N\n")
+pushConfirm = input()
+if pushConfirm == "n":
+    print("\nterminating...\n")
+    time.sleep(1)
+    quit()
 print("\ngit push\n")
 time.sleep(0.5)
 os.system("\ngit push")
 time.sleep(0.5)
+
 print("\n...Finished\n")
 time.sleep(0.5)

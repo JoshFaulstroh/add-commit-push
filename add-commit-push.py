63908753c156cd1a1dfbb1d0dc51bf1a9ed77e54
JoshFaulstroh <joshfaulstroh@gmail.com>
2023-11-04 19:16:54 -0500

    y

--- add-commit-push.py
+++ add-commit-push.py
@@ -8,17 +8,17 @@ numOfArgs = len(sys.argv)
 print("Total Arguments Passed:", numOfArgs)
 
 os.system("git status")
-print("Continue? type 'y' for yes, 'n' for no.")
+print("Continue? Y or N")
 confirm = input()
 if confirm == "n":
     print("terminating...")
     quit()
 
 message = input("Please provide commit message:")
-print("Commit message entered:", message, "Is this correct?", "Y or N")
+print("Commit message entered:", message, "Is this correct? Y or N")
 messageconfirm = input()
-if messageconfirm == "n":
-    message
+
+
 commitM = '\ngit commit -m "' +  message + '"'
 os.system(commitM)
 os.system("git add .")
